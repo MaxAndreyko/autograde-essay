201f4ff2c7b92159132aedd0c212c91b155db536
MaxAndreyko <max_andreyko@mail.ru>
2024-01-07 20:25:33 +0300

    add reading credentials json file to environmental variable

--- autograde_essay/utils.py
+++ autograde_essay/utils.py
@@ -1,21 +1,34 @@
 import dvc.api
 import joblib
+import json
+import os
 import pandas as pd
+import logging
 
+log = logging.getLogger(__name__)
 
 def read_data(
-    data_path: str, repo, encoding: str = "ISO-8859-1", sep="\t"
+    data_path: str, repo, path2creds: str, encoding: str = "ISO-8859-1", sep="\t"
 ) -> pd.DataFrame:
     """Reads tabular data from DVC cloud storage
 
     Args:
         data_path (str): Git repository relative path to data
         repo (str): Git repository link
+        path2creds(str): Path to json file with Google Drive service account credentials
         encoding (str, optional): File encoding. Defaults to "ISO-8859-1".
 
     Returns:
         pd.DataFrame: Downloaded table as pandas dataframe
     """
+
+    # Add credentials to environment variable if json file exsists
+    try:
+        with open(path2creds) as f:
+            os.environ["GDRIVE_CREDENTIALS_DATA"] = json.dumps(json.load(f))
+    except FileNotFoundError as e:
+        log.error("Error: %s", e)
+    
     with dvc.api.open(data_path, repo=repo, encoding=encoding) as fd:
         data = pd.read_csv(fd, sep=sep)
     return data
